77b925328483fd1f5fab316a2bca38d686e16485
miiwnoii <miiwnoii21>
2024-04-19 08:59:05 +0700

    manage route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,19 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  {path: 'login', component: LoginComponent},
-  { path: 'question-list', loadChildren: () => import('./question-list/question-list.module').then(m => m.QuestionListModule) }
+  { path: 'login', component: LoginComponent },
+  {
+    path: 'question-list',
+    loadChildren: () =>
+      import('./question-list/question-list.module').then(
+        (m) => m.QuestionListModule
+      ),
+  },
+  { path: '**', redirectTo: 'login', pathMatch: 'full' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
