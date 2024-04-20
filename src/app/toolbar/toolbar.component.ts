bf7d2b7d37ac70d8df675a4fa022df87a9fe917c
miiwnoii <miiwnoii21>
2024-04-20 09:24:19 +0700

    add condition to display user name and logout button

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { StorageService } from '../services/storage/storage.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-toolbar',
@@ -9,7 +10,7 @@ import { StorageService } from '../services/storage/storage.service';
 export class ToolbarComponent {
   FullName : string = ''
   IsLogIn : boolean = false
-  constructor(private storageService: StorageService){
+  constructor(private storageService: StorageService, private router: Router){
 
   }
 
@@ -19,5 +20,10 @@ export class ToolbarComponent {
       this.FullName = `Hello, ${userInfo.fullName}`;
       this.IsLogIn = true
     }
+  }
+
+  logout(){
+    this.storageService.terminateUserAuth();
+    this.router.navigate(['/login']);
   }
 }
