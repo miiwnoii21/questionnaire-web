3055bbe52b626a547536d696ad0f902cc293a58a
miiwnoii <miiwnoii21>
2024-04-20 08:29:02 +0700

    add route to question list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { QuestionListComponent } from './question-list/question-list.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: 'question-list', component: QuestionListComponent},
   { path: '**', redirectTo: 'login', pathMatch: 'full' },
 ];
 
