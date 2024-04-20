aea24f38db96a8d745456957973ee716de7417a4
miiwnoii <miiwnoii21>
2024-04-20 10:39:14 +0700

    add getQuestionCategories method

--- src/app/services/questionnaire/questionnaire.service.ts
+++ src/app/services/questionnaire/questionnaire.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { StorageService } from '../storage/storage.service';
 
 
 const QUESTIONNAIRE_API = 'https://training-homework.calllab.net'
@@ -14,9 +15,17 @@ const httpOptions = {
 
 export class QuestionnaireService {
 
-  constructor(private client: HttpClient) { }
+  constructor(private client: HttpClient, private storageService: StorageService) { }
   
   login(username: string, password: string): Observable<any>{
     return this.client.post(QUESTIONNAIRE_API+'/v1/login', {username,password}, httpOptions)
+  }
+
+  getQuestionCategories(): Observable<any>{
+    let user = this.storageService.getUserAuth();
+    //user?.accessToken
+    httpOptions.headers = httpOptions.headers.set('Authorization',`Bearer ${user?.accessToken}`)
+    console.log(`httpOptions- ${JSON.stringify(httpOptions.headers)}`)
+    return this.client.get(QUESTIONNAIRE_API+'/v1/questions/categories', httpOptions);
   }
 }
