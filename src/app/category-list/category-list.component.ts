2ee4c305270765567ff9e084658d1f0437eb8e37
miiwnoii <miiwnoii21>
2024-04-21 14:35:28 +0700

    add query param when redirect to question list

--- src/app/category-list/category-list.component.ts
+++ src/app/category-list/category-list.component.ts
@@ -30,7 +30,7 @@ export class CategoryListComponent {
 
   selectCategory(categoryId: string) {
     console.log('selectCategory' + categoryId);
-    this.router.navigate(['/question-list']);
+    this.router.navigate(['/question-list'], {queryParams: {category: categoryId}});
     // this.questionnaireService.getQuestions(categoryId).subscribe({
     //   next: (response) => {
     //     response.data
--- src/app/question-list/question-list.component.ts
+++ src/app/question-list/question-list.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
+import { QuestionnaireService } from '../services/questionnaire/questionnaire.service';
+import { QuestionCategory } from '../models/question-category/question-category';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-question-list',
   templateUrl: './question-list.component.html',
   styleUrl: './question-list.component.css'
 })
-export class QuestionListComponent {
 
+export class QuestionListComponent {
+  //questions: QuestionCategory[] = [];
+  constructor(private questionnaireService: QuestionnaireService, private route: ActivatedRoute){
+
+  }
+
+  ngOnInit(){
+    console.log('test')
+    //this.route.snapshot
+    const categoryId = this.route.snapshot.queryParamMap.get('category')?? '';
+    console.log(JSON.stringify(categoryId))
+    this.questionnaireService.getQuestions(categoryId).subscribe({
+      next: (response) => {
+        console.log("getQuestions");
+        console.log(response);
+        if(response.isSuccess){
+          //this.questions = response.data
+          //console.log(JSON.stringify(this.questions))
+        }
+      }
+    })
+
+    // this.questionnaireService.getQuestions(categoryId).subscribe({
+    //   next: (response) => {
+    //     response.data
+    //   }
+    // });
+  }
+
+  // selectCategory(private categoryId: string){
+
+  // }
 }
